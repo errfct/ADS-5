83a72482e6f1280d9d3789830c94f4754229af5f
errfct <veronika.sokolova.07@mail.ru>
2026-04-17 19:16:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,41 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size];
+    int top;
+
+public:
+    TStack() : top(-1) {}
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+    bool isFull() const {
+        return top >= size - 1;
+    }
+
+    void push(T item) {
+        if (isFull()) {
+            throw std::string("Full");
+        }
+        arr[++top] = item;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::string("Empty");
+        }
+        return arr[top--];
+    }
+
+    T get() const {
+        if (isEmpty()) {
+            throw std::string("Empty");
+        }
+        return arr[top];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
